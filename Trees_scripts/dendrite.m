84cb12231427da2e3e487ae0ca76bececacfe2d2
jwrussert <jwrussert@gmail.com>
2019-04-01 14:21:09 -0700

    Reverse rgb to make red=hot blue=cold

--- Trees_scripts/dendrite.m
+++ Trees_scripts/dendrite.m
@@ -87,12 +87,12 @@ classdef dendrite < handle
                 overlay = (1/(high-low))*(overlay - low);
                 
                 % Map these normalized values to rbg by finding the
-                % distance of each value from 1 (r), 0.5 (g), and 0 (b).
+                % distance of each value from 0 (r), 0.5 (g), and 1 (b).
                 % This approach is from https://codereview.stackexchange.com/questions/64708/calculation-of-rgb-values-given-min-and-max-values
                                
                 % Plot values
                 for i = 1:obj.nodes
-                    plot(obj.X(i), obj.Y(i), 'o', 'MarkerFaceColor', [norm(overlay(i)-1) norm(overlay(i)-0.5) overlay(i)])
+                    plot(obj.X(i), obj.Y(i), 'o', 'MarkerFaceColor', [overlay(i) norm(overlay(i)-0.5) norm(overlay(i)-1)])
                 end
             end
             
